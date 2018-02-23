f30635d67b2aa8a27991335d643c54a15644d734
epedzich <moquedire@yahoo.com>
2018-02-23 11:56:25 +0100

    done

--- zadanie_rekrutacyjne_3.py
+++ zadanie_rekrutacyjne_3.py
@@ -3,20 +3,15 @@ filename = './triangle.txt'
 with open(filename, "r") as triangle:
     lines = []
     for line in triangle:
-        lines.append(line.lstrip())
+        lines.append([int(num) for num in line.strip().split(' ')])
 
-result = []
-
-for line in lines:
-    result.append([int(num) for num in line.split(' ')])
-
-for line in range(len(result) - 1):
-    last_line = result[-1]
-    second_to_last_line = result[-2]
+for line in range(len(lines) - 1):
+    last_line = lines[-1]
+    second_to_last_line = lines[-2]
 
     for i in range(len(second_to_last_line)):
         second_to_last_line[i] += max(last_line[i], last_line[i + 1])
-    result.pop(-1)
-    result[-1] = second_to_last_line
+    lines.pop(-1)
+    lines[-1] = second_to_last_line
 
-print(result[0][0])
+print(lines[0][0])
